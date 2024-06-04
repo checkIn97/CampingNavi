e21aeec5ca914128d1743bc65c019d87cc1e88e1
DESKTOP-0CFDG3Q\702-01 <sangho0413@naver.com>
2024-06-04 17:50:23 +0900

    #1 QnaComment 생성

---
+++ src/main/java/com/demo/campingnavi/domain/QnaComment.java
@@ -0,0 +1,41 @@
+package com.demo.campingnavi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class QnaComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int qcseq; // 문의 답변 번호
+
+    @ManyToOne
+    @JoinColumn(name = "aseq", nullable = false)
+    private Admin admin; // 관리자
+
+    @ManyToOne
+    @JoinColumn(name = "qseq", nullable = false)
+    private Qna qna; // 문의
+
+    @Column(length = 2000, nullable = false)
+    private String content; // 내용
+
+    @Temporal(value=TemporalType.TIMESTAMP)
+    @ColumnDefault("sysdate")
+    @Column(updatable=false)
+    private Date createdAt; // 생성일
+}
