672cc730616f25223bfdff2fde0a082a963b71a8
Sangho-Do <sangho0413@naver.com>
2024-06-18 03:13:55 +0900

    리뷰 리스트 ajax 적용

--- src/main/java/com/demo/campingnavi/dto/ReviewVo.java
+++ src/main/java/com/demo/campingnavi/dto/ReviewVo.java
@@ -12,10 +12,16 @@ import lombok.ToString;
 public class ReviewVo {
     private Review review;
     private int rcdCount;
+    private String createdAt;
+    private String modifiedAt = null;
 
     public ReviewVo(Review review, int rcdCount) {
         this.review = review;
         this.rcdCount = rcdCount;
+        this.createdAt = String.format(review.getCreatedAt().toString(), "yyyy-MM-dd HH:mm:ss").substring(0, 19);
+        if (review.getModifiedAt() != null) {
+            this.modifiedAt = String.format(review.getModifiedAt().toString(), "yyyy-MM-dd HH:mm:ss").substring(0, 19);
+        }
     }
 
 }
