45b22e7d3dcea9969b6c70872240ead40928aa15
DESKTOP-0CFDG3Q\702-01 <sangho0413@naver.com>
2024-06-20 15:23:06 +0900

    campDetail 리뷰 리스트에서 작성자 이름과 작성일 사이에 공백 추가

--- src/main/java/com/demo/campingnavi/controller/CampDetailController.java
+++ src/main/java/com/demo/campingnavi/controller/CampDetailController.java
@@ -152,7 +152,7 @@ public class CampDetailController {
             for (Review review : reviewList) {
                 reviewListHtml.append("<div class=\"userReview\">")
                         .append("<div class=\"userName\">")
-                        .append("<span>").append(review.getMember().getNickname()).append("</span>")
+                        .append("<span>").append(review.getMember().getNickname()).append("　　</span>")
                         .append("<span class=\"userDate\">").append(new SimpleDateFormat("yyyy-MM-dd").format(review.getCreatedAt())).append("</span>")
                         .append("</div>")
                         .append("<div class=\"userGrade\">");
