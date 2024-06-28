458aaa03f9eb2bcab56366b0dfa524615337eff4
donbacksu <ohdyeong@naver.com>
2024-06-28 16:03:24 +0900

    로딩페이지 프론트

--- src/main/java/com/demo/campingnavi/controller/HomeController.java
+++ src/main/java/com/demo/campingnavi/controller/HomeController.java
@@ -42,4 +42,9 @@ public class HomeController {
         System.out.println(nickname);
         return result;
     }
+
+    @RequestMapping("/loding")
+    public String loding() {
+        return "lodingPage";
+    }
 }
